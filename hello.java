e6f0c16ac8ae5222e612faf3c56e2bc35673d6e4
sheshu2906 <sheshnath.yadav@knoldus.com>
2023-02-20 22:49:14 +0530

    jenkins show commmit

--- hello.java
+++ hello.java
@@ -10,11 +10,10 @@ class hello
 public static void main(String args[])
 {
 	hello h = new hello();	
-<<<<<<< HEAD
-	System.out.println("hello its me sheshnath yadav  ");
-=======
-	System.out.println("hello its meshna yadapdated and cre");
->>>>>>> 9ad7d0c088039a908f1a8101141af2690a792a0d
+
+	
+	System.out.println("hello its me sheshnath yadav");
+
 }
 
 }
